c6d31f280d5aa546903df6959cb87b94c8eb405e
DilshanHarendra <harendracripto@gmail.com>
2021-09-25 17:12:21 +0530

    connect mongo db

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const dbURI =
+    "mongodb+srv://ssdassignment:ssdassigment1@@cluster0.lggig.mongodb.net/doctor_reservation?retryWrites=true&w=majority";
+
+mongoose
+    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {})
 
 
 //port server listens
 app.listen(3000, () => {
     console.log("Server Satrted at port 3000");
-});
+})
+.catch(err => console.log(err));
